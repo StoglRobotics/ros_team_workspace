12e4faa6f115a5999b2e2612975cfeb400cfecc0
Denis Štogl <denis@stogl.de>
2021-02-24 19:18:01 +0100

    some corrections

--- scripts/setup-repository.bash
+++ scripts/setup-repository.bash
@@ -55,10 +55,11 @@ git commit -m "RosTeamWS: package created with initial files"
 
 # TODO: Check if uses want to configure CI
 # Build type
-echo "Which type of repository server are you using:"
+echo "Which type of repository server are you using [1]:"
 echo "(1) GitHub"
-echo "(2) Gitlab"
+echo "(2) Gitlab (not available yet - TBD)"
 read choice
+choice=${choice:=1}
 
 #TODO: Add automatically git remote
 
