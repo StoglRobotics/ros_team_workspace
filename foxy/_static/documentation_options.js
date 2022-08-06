74415828bab51436e53ee35552a8c265001cdba1
destogl <destogl@users.noreply.github.com>
2022-08-06 10:59:56 +0000

    deploy: f8b8714cda45f8f80d4cbb37f2c47596cba11621

--- foxy/_static/documentation_options.js
+++ foxy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Jul 2022',
+    VERSION: 'Aug 2022',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
---
+++ master/_sources/tutorials/index.rst
@@ -0,0 +1,12 @@
+===========
+Tutorials
+===========
+
+These tutorials will quickly get you using the ROS Team Workspace.
+
+.. toctree::
+   :maxdepth: 2
+   :numbered:
+
+   quick-start.rst
+   setting_up_rtw.rst
