205a4e4e7a5014b0b17abe250c69465b8922f426
destogl <destogl@users.noreply.github.com>
2023-08-01 09:55:25 +0000

    deploy: 6de1af5752971cdb6137d7a9bff831275c454033

--- foxy/_static/documentation_options.js
+++ foxy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Jul 2023',
+    VERSION: 'Aug 2023',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
