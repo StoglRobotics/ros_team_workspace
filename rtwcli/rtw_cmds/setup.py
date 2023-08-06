137285cca2ac3eae7af9da91b65d44abe8ce2461
Dr. Denis <denis@stoglrobotics.de>
2023-08-06 19:58:30 +0200

    Update setup.py

--- rtwcli/rtw_cmds/setup.py
+++ rtwcli/rtw_cmds/setup.py
@@ -19,7 +19,7 @@ package_name = "rtw_cmds"
 
 setup(
     name=package_name,
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(exclude=["test"]),
     install_requires=["rtwcli", "click"],
     zip_safe=True,
