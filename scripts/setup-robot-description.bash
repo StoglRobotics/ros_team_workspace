9f9375dfd9dc311b3296ba96eb9d3c0a56045ddf
Manuel M. <mamueluth@gmail.com>
2024-02-08 13:23:33 +0100

    add first version of urdf test in python

--- scripts/setup-robot-description.bash
+++ scripts/setup-robot-description.bash
@@ -19,8 +19,9 @@
 usage="setup-robot-description ROBOT_NAME"
 
 # Load Framework defines
-script_own_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+script_own_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source $script_own_dir/../setup.bash
+
 check_and_set_ros_distro_and_version ${ROS_DISTRO}
 
 ROBOT_NAME=$1
@@ -34,7 +35,7 @@ fi
 PKG_NAME="$(grep -Po '(?<=<name>).*?(?=</name>)' package.xml | sed -e 's/[[:space:]]//g')"
 
 echo ""
-echo -e "${TERMINAL_COLOR_USER_NOTICE}ATTENTION: Setting up description configuration for robot '$ROBOT_NAME' in package '$PKG_NAME' in folder '`pwd`'.${TERMINAL_COLOR_NC}"
+echo -e "${TERMINAL_COLOR_USER_NOTICE}ATTENTION: Setting up description configuration for robot '$ROBOT_NAME' in package '$PKG_NAME' in folder '$(pwd)'.${TERMINAL_COLOR_NC}"
 echo -e "${TERMINAL_COLOR_USER_CONFIRMATION}If correct press <ENTER>, otherwise <CTRL>+C and start the script again from the package folder and/or with correct robot name.${TERMINAL_COLOR_NC}"
 read
 
@@ -74,41 +75,81 @@ mkdir -p rviz
 ROBOT_RVIZ="rviz/${ROBOT_NAME}.rviz"
 cp -n "$ROBOT_DESCRIPTION_TEMPLATES/robot.rviz" $ROBOT_RVIZ
 
-# sed all needed files
-FILES_TO_SED=($ROBOT_URDF_XACRO $ROBOT_MACRO $ROBOT_MACRO_ROS2_CONTROL $VIEW_ROBOT_LAUNCH)
+# copy test files
+mkdir -p test
+ROBOT_TEST_FILE="test/${ROBOT_NAME}_test_urdf_xacro.py"
+cp -n "${ROBOT_DESCRIPTION_TEMPLATES}/test_urdf_xacro.py" $ROBOT_TEST_FILE
 
+# sed all needed files
+FILES_TO_SED=($ROBOT_URDF_XACRO $ROBOT_MACRO $ROBOT_MACRO_ROS2_CONTROL $VIEW_ROBOT_LAUNCH $ROBOT_TEST_FILE)
 for SED_FILE in "${FILES_TO_SED[@]}"; do
   sed -i "s/\\\$PKG_NAME\\\$/${PKG_NAME}/g" $SED_FILE
   sed -i "s/\\\$ROBOT_NAME\\\$/${ROBOT_NAME}/g" $SED_FILE
 done
 
-# Add dependencies if they not exist
-DEP_PKGS=("xacro" "rviz2" "robot_state_publisher" "joint_state_publisher_gui")
-
-for DEP_PKG in "${DEP_PKGS[@]}"; do
-  if `grep -q $DEP_PKG package.xml`; then
+# Add all the exec depend packages
+DEPENDENCIES=("joint_state_publisher_gui" "robot_state_publisher" "rviz2" "xacro")
+PREVIOUS_STRING="<buildtool_depend>ament_cmake<\/buildtool_depend>"
+DEPEND_TAG="exec_depend"
+for DEP_PKG in "${DEPENDENCIES[@]}"; do
+  # Check if the current package is already listed in the package.xml file
+  if grep -q "<$DEPEND_TAG>${DEP_PKG}<\/$DEPEND_TAG>" package.xml; then
     echo "'$DEP_PKG' is already listed in package.xml"
   else
-    append_to_string="<buildtool_depend>ament_cmake<\/buildtool_depend>"
-    sed -i "s/$append_to_string/$append_to_string\\n\\n  <exec_depend>${DEP_PKG}<\/exec_depend>/g" package.xml
+    # Check if the previous string starts with <buildtool_depend>
+    if [[ "$PREVIOUS_STRING" != \<$DEPEND_TAG* ]]; then
+      # If the previous string starts with different tag, add 2 newline characters after it to start new block
+      newline="\n\n"
+    else
+      # otherwise add only one newline character so it's in the same block
+      newline="\n"
+    fi
+    sed -i "s#$PREVIOUS_STRING#$PREVIOUS_STRING${newline}  <$DEPEND_TAG>${DEP_PKG}<\/$DEPEND_TAG>#g" package.xml
+    PREVIOUS_STRING="<$DEPEND_TAG>${DEP_PKG}<\/$DEPEND_TAG>"
   fi
 done
 
+# Add all the test depend packages
+DEPENDENCIES=("ament_cmake_pytest" "launch_testing_ament_cmake" "launch_testing_ros" "liburdfdom-tools" "xacro")
+PREVIOUS_STRING="<test_depend>ament_lint_common</test_depend>"
+DEPEND_TAG="test_depend"
+for DEP_PKG in "${DEPENDENCIES[@]}"; do
+  # Check if the current package is already listed in the package.xml file
+  if grep -q "<$DEPEND_TAG>${DEP_PKG}<\/$DEPEND_TAG>" package.xml; then
+    echo "'$DEP_PKG' is already listed in package.xml"
+  else
+    # Check if the previous string starts with <buildtool_depend>
+    if [[ "$PREVIOUS_STRING" != \<$DEPEND_TAG* ]]; then
+      # If the previous string starts with different tag, add 2 newline characters after it to start new block
+      newline="\n\n"
+    else
+      # otherwise add only one newline character so it's in the same block
+      newline="\n"
+    fi
+    sed -i "s#$PREVIOUS_STRING#$PREVIOUS_STRING${newline}  <$DEPEND_TAG>${DEP_PKG}<\/$DEPEND_TAG>#g" package.xml
+    PREVIOUS_STRING="<$DEPEND_TAG>${DEP_PKG}<\/$DEPEND_TAG>"
+  fi
+done
+
+# Update the CMakeLists.txt
 # Add install paths of the files
 preppend_to_string="if(BUILD_TESTING)"
-sed -i "s/$preppend_to_string/install\(\\n  DIRECTORY config launch meshes rviz urdf\\n  DESTINATION share\/\$\{PROJECT_NAME\}\\n\)\\n\\n$preppend_to_string/g" CMakeLists.txt
+sed -i "s/$preppend_to_string/install\(\\n  DIRECTORY config launch meshes rviz urdf test\\n  DESTINATION share\/\$\{PROJECT_NAME\}\\n\)\\n\\n$preppend_to_string/g" CMakeLists.txt
+# Add the test
+lines_to_append="  find_package(ament_cmake_pytest REQUIRED)\n  ament_add_pytest_test(test_${ROBOT_NAME}_urdf_xacro ${ROBOT_TEST_FILE})"
+# Define the search pattern
+pattern='if(BUILD_TESTING)'
+# Use sed to find the pattern and append the lines after it in CMakeLists.txt
+sed -i "/$pattern/a$lines_to_append" CMakeLists.txt
 
 # extend README with general instructions
 if [ -f README.md ]; then
-  cat $ROBOT_DESCRIPTION_TEMPLATES/append_to_README.md >> README.md
+  cat $ROBOT_DESCRIPTION_TEMPLATES/append_to_README.md >>README.md
   sed -i "s/\\\$PKG_NAME\\\$/${PKG_NAME}/g" README.md
   sed -i "s/\\\$ROBOT_NAME\\\$/${ROBOT_NAME}/g" README.md
 fi
 
 #TODO: Set license
-
-git add .
-git commit -m "RosTeamWS: Description files for $ROBOT_NAME generated."
 
 # Compile and add new package the to the path
 compile_and_source_package $PKG_NAME
