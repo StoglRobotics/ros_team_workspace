5f88f409e7df9e400c55f604c0a12a3a5acfc092
Daniel Azanov <daniel.azanov@stoglrobotics.de>
2024-06-10 16:17:21 +0200

    [cli] move argcomplete to install_requires

--- rtwcli/rtwcli/setup.py
+++ rtwcli/rtwcli/setup.py
@@ -19,10 +19,7 @@ setup(
     name="rtwcli",
     version="0.2.0",
     packages=find_packages(exclude=["test"]),
-    install_requires=["docker", "questionary", "rocker"],
-    extras_require={
-        "completion": ["argcomplete"],
-    },
+    install_requires=["argcomplete", "docker", "questionary", "rocker"],
     zip_safe=False,
     keywords=[],
     classifiers=[
