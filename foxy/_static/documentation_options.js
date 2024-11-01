d89d9a7f7622fa9a43acbcb9d621e7c14ad043c6
muritane <muritane@users.noreply.github.com>
2024-11-01 15:14:15 +0000

    deploy: 2860b2db66c8098951b507df15cea20dfd3eacec

--- foxy/_static/documentation_options.js
+++ foxy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Oct 2024',
+    VERSION: 'Nov 2024',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
