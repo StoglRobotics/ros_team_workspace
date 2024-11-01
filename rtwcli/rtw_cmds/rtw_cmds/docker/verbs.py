0639c4bd36c2b515b81db934590ab2c25c31f139
Daniel Azanov <daniel.azanov@stoglrobotics.de>
2024-11-01 15:10:48 +0100

    [rtwcli] rtw docker restart/start/stop

--- rtwcli/rtw_cmds/rtw_cmds/docker/verbs.py
+++ rtwcli/rtw_cmds/rtw_cmds/docker/verbs.py
@@ -16,11 +16,61 @@ from rtwcli import logger
 from rtwcli.docker_utils import (
     docker_exec_interactive_bash,
     docker_start,
+    docker_stop,
     fix_missing_xauth_file,
     is_docker_container_running,
 )
 from rtwcli.verb import VerbExtension
-from rtwcli.workspace_utils import get_current_workspace
+from rtwcli.workspace_utils import Workspace, get_current_workspace
+
+
+def check_workspace(ws: Workspace) -> bool:
+    if not ws:
+        logger.info("No workspace is active.")
+        return False
+
+    if not ws.ws_docker_support:
+        logger.info("The workspace does not support docker.")
+        return False
+
+    if not ws.docker_container_name:
+        logger.error("The workspace is missing the docker_container_name attribute.")
+        return False
+
+    return True
+
+
+def container_start_routine(ws: Workspace) -> bool:
+    if not check_workspace(ws):
+        return False
+
+    if not is_docker_container_running(ws.docker_container_name):
+        logger.info(
+            f"The docker container '{ws.docker_container_name}' is not running, starting it now."
+        )
+        # fix missing .xauth file if it is not present
+        if not fix_missing_xauth_file(ws.docker_container_name):
+            logger.error(f"Failed to fix missing .xauth file for '{ws.docker_container_name}'.")
+            return False
+
+        if not docker_start(ws.docker_container_name):
+            logger.error(f"Failed to start docker container '{ws.docker_container_name}'.")
+            return False
+
+    return True
+
+
+def container_stop_routine(ws: Workspace) -> bool:
+    if not check_workspace(ws):
+        return False
+
+    if is_docker_container_running(ws.docker_container_name):
+        logger.info(f"Stopping docker container '{ws.docker_container_name}'.")
+        if not docker_stop(ws.docker_container_name):
+            logger.error(f"Failed to stop docker container '{ws.docker_container_name}'.")
+            return False
+
+    return True
 
 
 class EnterVerb(VerbExtension):
@@ -28,31 +78,30 @@ class EnterVerb(VerbExtension):
 
     def main(self, *, args):
         ws = get_current_workspace()
-        if not ws:
-            logger.info("No workspace is active.")
+        if not container_start_routine(ws):
             return
-
-        if not ws.ws_docker_support:
-            logger.info("The workspace does not support docker.")
-            return
-
-        if not ws.docker_container_name:
-            logger.error("The workspace is missing the docker_container_name attribute.")
-            return
-
-        if not is_docker_container_running(ws.docker_container_name):
-            logger.info(
-                f"The docker container '{ws.docker_container_name}' is not running, starting it now."
-            )
-            # fix missing .xauth file if it is not present
-            if not fix_missing_xauth_file(ws.docker_container_name):
-                logger.error(
-                    f"Failed to fix missing .xauth file for '{ws.docker_container_name}'."
-                )
-                return
-
-            if not docker_start(ws.docker_container_name):
-                logger.error(f"Failed to start docker container '{ws.docker_container_name}'.")
-                return
-
         docker_exec_interactive_bash(ws.docker_container_name)
+
+
+class StartVerb(VerbExtension):
+    """Start docker container."""
+
+    def main(self, *, args):
+        container_start_routine(get_current_workspace())
+
+
+class StopVerb(VerbExtension):
+    """Stop docker container."""
+
+    def main(self, *, args):
+        container_stop_routine(get_current_workspace())
+
+
+class RestartVerb(VerbExtension):
+    """Restart docker container."""
+
+    def main(self, *, args):
+        ws = get_current_workspace()
+        if not container_stop_routine(ws):
+            return
+        container_start_routine(ws)
